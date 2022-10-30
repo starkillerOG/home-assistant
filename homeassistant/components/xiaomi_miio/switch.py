b1df3f092a27168107ef783b1528a51824159be9
Teemu Rytilahti <tpr@iki.fi>
2022-10-30 21:58:13 +0100

    tidy switch

--- homeassistant/components/xiaomi_miio/switch.py
+++ homeassistant/components/xiaomi_miio/switch.py
@@ -3,12 +3,10 @@ from __future__ import annotations
 
 import logging
 
-import voluptuous as vol
+from miio.descriptors import SettingType
 
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import ATTR_ENTITY_ID
 from homeassistant.core import HomeAssistant
-import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from .const import DOMAIN, KEY_COORDINATOR, KEY_DEVICE
@@ -18,16 +16,6 @@ _LOGGER = logging.getLogger(__name__)
 
 DEFAULT_NAME = "Xiaomi Miio Switch"
 DATA_KEY = "switch.xiaomi_miio"
-
-
-KEY_CHANNEL = "channel"
-GATEWAY_SWITCH_VARS = {
-    "status_ch0": {KEY_CHANNEL: 0},
-    "status_ch1": {KEY_CHANNEL: 1},
-    "status_ch2": {KEY_CHANNEL: 2},
-}
-
-SERVICE_SCHEMA = vol.Schema({vol.Optional(ATTR_ENTITY_ID): cv.entity_ids})
 
 
 async def async_setup_entry(
@@ -44,10 +32,10 @@ async def async_setup_entry(
     if DATA_KEY not in hass.data:
         hass.data[DATA_KEY] = {}
 
-    # Handle switches defined by the backing class.
-    switches = device.switches()
-    _LOGGER.debug("Found switches: %s", len(device.switches()))
-    for switch in switches.values():
+    switches = filter(
+        lambda x: x.type == SettingType.Boolean, device.settings().values()
+    )
+    for switch in switches:
         _LOGGER.info("Adding switch: %s", switch)
         entities.append(XiaomiSwitch(device, switch, config_entry, coordinator))
 
