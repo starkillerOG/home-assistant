b981363eb541f5043545fa3252222de1ee016859
starkillerOG <starkiller.og@gmail.com>
2023-10-29 14:19:58 +0100

    initial xannor

--- homeassistant/components/reolink/camera.py
+++ homeassistant/components/reolink/camera.py
@@ -1,16 +1,21 @@
 """Component providing support for Reolink IP cameras."""
 from __future__ import annotations
 
+import datetime as dt
 import logging
 
+from aiohttp import web
+from aiohttp.typedefs import LooseHeaders
 from reolink_aio.api import DUAL_LENS_MODELS
+from reolink_aio.exceptions import InvalidContentTypeError
+from reolink_aio.typings import VOD_download
 
 from homeassistant.components.camera import Camera, CameraEntityFeature
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
-from . import ReolinkData
+from . import ReolinkData, view as view_module
 from .const import DOMAIN
 from .entity import ReolinkChannelCoordinatorEntity
 
@@ -25,6 +30,11 @@ async def async_setup_entry(
     """Set up a Reolink IP Camera."""
     reolink_data: ReolinkData = hass.data[DOMAIN][config_entry.entry_id]
     host = reolink_data.host
+
+    if not hass.data[DOMAIN].get("views_configured", False) and hass.data[
+        DOMAIN
+    ].setdefault("views_configured", True):
+        view_module.async_setup_views(hass)
 
     cameras = []
     for channel in host.api.stream_channels:
@@ -44,6 +54,53 @@ async def async_setup_entry(
             cameras.append(ReolinkCamera(reolink_data, channel, stream))
 
     async_add_entities(cameras)
+
+
+class ReolinkDownloadResponse(web.StreamResponse):
+    """Stream repeater similar to web.FileResponse."""
+
+    def __init__(
+        self,
+        vod: VOD_download,
+        status: int = 200,
+        reason: str | None = None,
+        headers: LooseHeaders | None = None,
+    ) -> None:
+        """Init Response."""
+        super().__init__(status=status, reason=reason, headers=headers)
+        self._vod = vod
+
+    async def _send_vod(self, request: web.BaseRequest):
+        writer = await super().prepare(request)
+        assert writer is not None
+
+        transport = request.transport
+        assert transport is not None
+
+        vod = self._vod
+        async for chunk in vod.stream.iter_any():
+            if transport.is_closing():
+                _LOGGER.debug("Client closed stream, aborting download")
+                break
+            await writer.write(chunk)
+
+        _LOGGER.debug("Closing VOD")
+        vod.close()
+        await writer.drain()
+        return writer
+
+    async def prepare(self, request: web.BaseRequest):
+        """Prepare response."""
+
+        _LOGGER.debug("Preparing VOD for download (%s)", self._vod.filename)
+        vod = self._vod
+        if vod.etag:
+            self.etag = vod.etag.replace('"', "")
+        self.content_length = vod.length
+
+        writer = await self._send_vod(request)
+        await writer.write_eof()
+        return writer
 
 
 class ReolinkCamera(ReolinkChannelCoordinatorEntity, Camera):
@@ -83,3 +140,69 @@ class ReolinkCamera(ReolinkChannelCoordinatorEntity, Camera):
     ) -> bytes | None:
         """Return a still image response from the camera."""
         return await self._host.api.get_snapshot(self._channel, self._stream)
+
+    async def async_get_camera_time(self):
+        """Get camera time."""
+        if now := self._host.api.time():
+            return now
+        now = await self._host.api.async_get_time()
+        return now
+
+    async def async_get_timezone(self):
+        """Get camera tzinfo."""
+        if tz := self._host.api.timezone():
+            return tz
+        tz = (await self.async_get_camera_time()).tzinfo
+        return tz
+
+    async def handle_async_download_search(
+        self,
+        start: dt.datetime | dt.date | None = None,
+        end: dt.datetime | dt.date | None = None,
+        status_only: bool = False,
+    ):
+        """Get Camera VODs between start and end date."""
+
+        if start is None:
+            _start = await self.async_get_camera_time()
+        elif not isinstance(start, dt.datetime):
+            ct = await self.async_get_camera_time()
+            _start = dt.datetime.combine(
+                start, ct.time() if ct.date() == start else dt.time.min, ct.tzinfo
+            )
+        elif start.tzinfo is None:
+            _start = start.replace(tzinfo=await self.async_get_timezone())
+        else:
+            _start = start
+
+        if end is None:
+            _end = _start
+            _start = dt.datetime.combine(_start.date(), dt.time.min, _start.tzinfo)
+        elif not isinstance(end, dt.datetime):
+            ct = await self.async_get_camera_time()
+            _end = dt.datetime.combine(
+                end, ct.time() if ct.date() == end else dt.time.max, ct.tzinfo
+            )
+        elif end.tzinfo is None:
+            _end = end.replace(tzinfo=_start.tzinfo)
+        else:
+            _end = end
+
+        if _start > _end:
+            [_start, _end] = [_end, _start]
+
+        return await self._host.api.request_vod_files(
+            self._channel, _start, _end, status_only, "main"  # self._stream
+        )
+
+    async def handle_async_download_stream(self, filename: str) -> web.StreamResponse:
+        """Generate a Download Response to a requested file."""
+
+        try:
+            vod = await self._host.api.download_vod(filename)
+        except InvalidContentTypeError as exc:
+            raise web.HTTPServerError(
+                reason="Cannot download multiple files at once."
+            ) from exc
+
+        return ReolinkDownloadResponse(vod)
