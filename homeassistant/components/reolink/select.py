3588b764032d955a144f122ac731c67e11daa7d8
starkillerOG <starkiller.og@gmail.com>
2024-06-01 13:57:49 +0200

    Adjust name of doorbell LED

--- homeassistant/components/reolink/select.py
+++ homeassistant/components/reolink/select.py
@@ -109,7 +109,7 @@ SELECT_ENTITIES = (
     ReolinkSelectEntityDescription(
         key="status_led",
         cmd_key="GetPowerLed",
-        translation_key="status_led",
+        translation_key="doorbell_led",
         entity_category=EntityCategory.CONFIG,
         get_options=lambda api, ch: api.doorbell_led_list(ch),
         supported=lambda api, ch: api.supported(ch, "doorbell_led"),
