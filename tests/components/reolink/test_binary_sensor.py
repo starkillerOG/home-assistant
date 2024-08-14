b6153888ec6916937339e19963ec04dd1e5ce111
starkillerOG <starkiller.og@gmail.com>
2024-08-14 10:12:23 +0200

    Split out host test

--- tests/components/reolink/test_binary_sensor.py
+++ tests/components/reolink/test_binary_sensor.py
@@ -25,6 +25,7 @@ from tests.typing import ClientSessionGenerator
 
 async def test_motion_sensor(
     hass: HomeAssistant,
+    hass_client_no_auth: ClientSessionGenerator,
     config_entry: MockConfigEntry,
     reolink_connect: MagicMock,
     entity_registry: er.EntityRegistry,
@@ -47,71 +48,12 @@ async def test_motion_sensor(
     await hass.async_block_till_done()
 
     assert hass.states.is_state(entity_id, "off")
-
-
-async def test_webhook_callback(
-    hass: HomeAssistant,
-    hass_client_no_auth: ClientSessionGenerator,
-    config_entry: MockConfigEntry,
-    reolink_connect: MagicMock,
-    entity_registry: er.EntityRegistry,
-) -> None:
-    """Test webhook callback with motion sensor."""
+    
+    # test webhook callback
     reolink_connect.motion_detected.return_value = True
-    with patch("homeassistant.components.reolink.PLATFORMS", [Platform.BINARY_SENSOR]):
-        assert await hass.config_entries.async_setup(config_entry.entry_id) is True
-    await hass.async_block_till_done()
-    assert config_entry.state is ConfigEntryState.LOADED
-
-    entity_id = f"{Platform.BINARY_SENSOR}.{TEST_NVR_NAME}_motion"
-    assert hass.states.is_state(entity_id, "on")
-
-    webhook_id = f"{const.DOMAIN}_{TEST_UID.replace(':', '')}_ONVIF"
-
-    signal_all = MagicMock()
-    signal_ch = MagicMock()
-    async_dispatcher_connect(hass, f"{webhook_id}_all", signal_all)
-    async_dispatcher_connect(hass, f"{webhook_id}_0", signal_ch)
-
-    client = await hass_client_no_auth()
-
-    # test webhook callback success all channels
-    reolink_connect.ONVIF_event_callback.return_value = None
-    await client.post(f"/api/webhook/{webhook_id}")
-    signal_all.assert_called_once()
-
-    # test webhook callback all channels with failure to read motion_state
-    reolink_connect.get_motion_state_all_ch.return_value = False
-    await client.post(f"/api/webhook/{webhook_id}")
-    signal_all.assert_called_once()
-
-    # test webhook callback success single channel
     reolink_connect.ONVIF_event_callback.return_value = [0]
+    webhook_id = f"{const.DOMAIN}_{TEST_UID.replace(':', '')}_ONVIF"
+    client = await hass_client_no_auth()
     await client.post(f"/api/webhook/{webhook_id}", data="test_data")
-    signal_ch.assert_called_once()
-
-    # test webhook callback single channel with error in event callback
-    reolink_connect.ONVIF_event_callback = AsyncMock(
-        side_effect=Exception("Test error")
-    )
-    await client.post(f"/api/webhook/{webhook_id}", data="test_data")
-    signal_ch.assert_called_once()
-
-    # test failure to read date from webhook post
-    request = MockRequest(
-        method="POST",
-        content=bytes("test", "utf-8"),
-        mock_source="test",
-    )
-    request.read = AsyncMock(side_effect=ConnectionResetError("Test error"))
-    await async_handle_webhook(hass, webhook_id, request)
-    signal_all.assert_called_once()
-
-    request.read = AsyncMock(side_effect=ClientResponseError("Test error", "Test"))
-    await async_handle_webhook(hass, webhook_id, request)
-    signal_all.assert_called_once()
-
-    request.read = AsyncMock(side_effect=CancelledError("Test error"))
-    with pytest.raises(CancelledError):
-        await async_handle_webhook(hass, webhook_id, request)
-    signal_all.assert_called_once()
+    
+    assert hass.states.is_state(entity_id, "on")
