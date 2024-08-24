f4b530378a80cc0c27ba5189989b2ef6738be3f2
starkillerOG <starkiller.og@gmail.com>
2024-08-24 11:33:55 +0200

    Improve host test coverage

--- homeassistant/components/reolink/host.py
+++ homeassistant/components/reolink/host.py
@@ -437,7 +437,15 @@ class ReolinkHost:
             self._long_poll_task.cancel()
             self._long_poll_task = None
 
-        await self._api.unsubscribe(sub_type=SubType.long_poll)
+        try:
+            await self._api.unsubscribe(sub_type=SubType.long_poll)
+        except ReolinkError as err:
+            _LOGGER.error(
+                "Reolink error while unsubscribing from host %s:%s: %s",
+                self._api.host,
+                self._api.port,
+                err,
+            )
 
     async def stop(self, event=None) -> None:
         """Disconnect the API."""
--- tests/components/reolink/test_host.py
+++ tests/components/reolink/test_host.py
@@ -2,11 +2,13 @@
 
 from asyncio import CancelledError
 from unittest.mock import AsyncMock, MagicMock
-
+from freezegun.api import FrozenDateTimeFactory
+from reolink_aio.exceptions import NotSupportedError, ReolinkError, SubscriptionError
 from aiohttp import ClientResponseError
 import pytest
-
+from datetime import timedelta
 from homeassistant.components.reolink import const
+from homeassistant.components.reolink.host import FIRST_ONVIF_TIMEOUT
 from homeassistant.components.webhook import async_handle_webhook
 from homeassistant.config_entries import ConfigEntryState
 from homeassistant.core import HomeAssistant
@@ -16,13 +18,14 @@ from homeassistant.util.aiohttp import MockRequest
 
 from .conftest import TEST_UID
 
-from tests.common import MockConfigEntry
+from tests.common import MockConfigEntry, async_fire_time_changed
 from tests.typing import ClientSessionGenerator
 
 
 async def test_webhook_callback(
     hass: HomeAssistant,
     hass_client_no_auth: ClientSessionGenerator,
+    freezer: FrozenDateTimeFactory,
     config_entry: MockConfigEntry,
     reolink_connect: MagicMock,
     entity_registry: er.EntityRegistry,
@@ -45,6 +48,10 @@ async def test_webhook_callback(
     reolink_connect.ONVIF_event_callback.return_value = None
     await client.post(f"/api/webhook/{webhook_id}")
     signal_all.assert_called_once()
+
+    freezer.tick(timedelta(seconds=FIRST_ONVIF_TIMEOUT))
+    async_fire_time_changed(hass)
+    await hass.async_block_till_done()
 
     # test webhook callback all channels with failure to read motion_state
     signal_all.reset_mock()
@@ -83,3 +90,78 @@ async def test_webhook_callback(
     with pytest.raises(CancelledError):
         await async_handle_webhook(hass, webhook_id, request)
     signal_all.assert_not_called()
+
+
+async def test_no_mac(
+    hass: HomeAssistant,
+    config_entry: MockConfigEntry,
+    reolink_connect: MagicMock,
+) -> None:
+    """Test setup of host with no mac."""
+    reolink_connect.mac_address = None
+    assert not await hass.config_entries.async_setup(config_entry.entry_id)
+    await hass.async_block_till_done()
+    assert config_entry.state is ConfigEntryState.SETUP_RETRY
+
+
+async def test_subscribe_error(
+    hass: HomeAssistant,
+    config_entry: MockConfigEntry,
+    reolink_connect: MagicMock,
+) -> None:
+    """Test error handeling when subscribing to ONVIF."""
+    reolink_connect.subscribe.side_effect = ReolinkError("Test Error")
+    reolink_connect.subscribed.return_value = False
+    assert await hass.config_entries.async_setup(config_entry.entry_id)
+    await hass.async_block_till_done()
+    assert config_entry.state is ConfigEntryState.LOADED
+
+
+async def test_subscribe_unsuccesfull(
+    hass: HomeAssistant,
+    config_entry: MockConfigEntry,
+    reolink_connect: MagicMock,
+) -> None:
+    """Test setup when subscribing to ONVIF is unsuccesfull."""
+    reolink_connect.subscribed.return_value = False
+    assert await hass.config_entries.async_setup(config_entry.entry_id)
+    await hass.async_block_till_done()
+    assert config_entry.state is ConfigEntryState.LOADED
+    
+async def test_initial_ONVIF_not_supported(
+    hass: HomeAssistant,
+    config_entry: MockConfigEntry,
+    reolink_connect: MagicMock,
+) -> None:
+    """Test setup when initial ONVIF is not supported."""
+    def test_supported(ch, key):
+        """Test supported function."""
+        if key == "initial_ONVIF_state":
+            return False
+        return True
+    
+    reolink_connect.supported = test_supported
+
+    assert await hass.config_entries.async_setup(config_entry.entry_id)
+    await hass.async_block_till_done()
+    assert config_entry.state is ConfigEntryState.LOADED
+
+async def test_ONVIF_not_supported(
+    hass: HomeAssistant,
+    config_entry: MockConfigEntry,
+    reolink_connect: MagicMock,
+) -> None:
+    """Test setup when ONVIF is not supported."""
+    def test_supported(ch, key):
+        """Test supported function."""
+        if key == "initial_ONVIF_state":
+            return False
+        return True
+    
+    reolink_connect.supported = test_supported
+    reolink_connect.subscribed.return_value = False
+    reolink_connect.subscribe.side_effect = NotSupportedError("Test error")
+
+    assert await hass.config_entries.async_setup(config_entry.entry_id)
+    await hass.async_block_till_done()
+    assert config_entry.state is ConfigEntryState.LOADED
